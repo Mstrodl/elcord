de95846fd509a5128b15cc78b033d66a7e66d002
heatingdevice <ipadlover8322@gmail.com>
2017-12-04 15:19:55 -0500

    MELPA wants this stuff

--- elcord.el
+++ elcord.el
@@ -9,13 +9,12 @@
 ;;; Code:
 
 (provide 'elcord)
-(setq debug-on-error t)
 
 ;;; Commentary:
 ;; elcord allows you to show off your buffer with all your Discord friends via the new rich presence feature
 
 (require 'json)
-(eval-when-compile (require 'cl))
+(eval-when-compile (require 'cl-lib))
 (require 'bindat)
 
 
@@ -24,8 +23,6 @@
   "Debug function used to log connection state change.
 Argument PROCESS Process the event occured on.
 Argument EVNT The reason the connection state changed."
-  (setf print-escape-newlines t)
-  
   (if (string= evnt "connection broken by remote peer\n")
       (elcord-handle-disconnect)
     (message (prin1-to-string evnt)))
@@ -39,8 +36,8 @@ Argument EVNT The reason the connection state changed."
 
 (defun elcord-handle-disconnect ()
   "Handles reconnecting when socket disconnects..."
-  (setf elcord-connected nil)
-  (setf elcord-first-message nil)
+  (setq elcord-connected nil)
+  (setq elcord-first-message nil)
   (elcord-connect))
 
 (defvar elcord-first-message nil)
@@ -50,10 +47,10 @@ Argument EVNT The reason the connection state changed."
 Argument PROCESS The process the message was recieved on.
 Argument EVNT The message recieved on the socket."
   ; (message evnt)
-  (if (not elcord-first-message)
+  (unless elcord-first-message
       (progn
         (elcord-create-presence)
-        (setf elcord-first-message t))))
+        (setq elcord-first-message t))))
 
 (defvar elcord-jsonstr "")
 (defvar elcord-datalen 0)
@@ -64,13 +61,13 @@ Argument EVNT The message recieved on the socket."
   "Packs and sends a packet to the IPC server.
 Argument OPCODE OP code to send.
 Argument OBJ The data to send to the IPC server."
-  (setf elcord-jsonstr (json-encode obj))
-  (setf elcord-datalen (length elcord-jsonstr))
-  (setf elcord-message-spec
+  (setq elcord-jsonstr (json-encode obj))
+  (setq elcord-datalen (length elcord-jsonstr))
+  (setq elcord-message-spec
     `((:op u32r)
       (:len u32r)
       (:data str ,elcord-datalen)))
-  (setf elcord-packet (bindat-pack
+  (setq elcord-packet (bindat-pack
                   elcord-message-spec
                   `((:op . ,opcode)
                     (:len . ,elcord-datalen)
@@ -89,7 +86,7 @@ Argument OBJ The data to send to the IPC server."
 Argument FILENAME Name of current buffer.
 Argument LINE-NUM Line number the pointer is located at.
 Argument LINE-COUNT Total number of lines in buffer."
-  (setf elcord-activity `(
+  (setq elcord-activity `(
                    ("assets" . (
                                 ("large_image" . ,(if (boundp 'spacemacs-version) "spacemacs_icon" "emacs_icon"))
                                 ("large_text" . "Use this!")
@@ -107,8 +104,8 @@ Argument LINE-COUNT Total number of lines in buffer."
                                   ("spectate" . "stupidvimuseruseemacs"))
                                    '(("match" . "emacsisbest"))))
                    ))
-  (setf elcord-nonce (format-time-string "%s%N"))
-  (setf elcord-presence `(
+  (setq elcord-nonce (format-time-string "%s%N"))
+  (setq elcord-presence `(
                    ("cmd" . "SET_ACTIVITY")
                    ("args" . (("activity" . ,elcord-activity)
                               ("pid" . ,elcord-pid)))
@@ -119,16 +116,16 @@ Argument LINE-COUNT Total number of lines in buffer."
 
 (defvar elcord-discord-socket (concat (or (getenv "XDG_RUNTIME_DIR") (getenv "TMPDIR") (getenv "TMP") (getenv "TEMP") "/tmp") "/discord-ipc-0"))
 
-(if (eq system-type "windows-nt")
-    (setf elcord-discord-socket "\\\\?\\pipe\\discord-ipc-0"))
+(when (eq system-type 'windows-nt)
+    (setq elcord-discord-socket "\\\\?\\pipe\\discord-ipc-0"))
 
 (defvar elcord-sock nil)
 (defun elcord-connect ()
   "Connects to the Discord socket."
   (condition-case err
-    (if (not elcord-connected)
+      (unless elcord-connected
         (progn
-          (setf elcord-sock
+          (setq elcord-sock
                 (make-network-process :name "elcord-sock"
                                       :remote elcord-discord-socket
                                       :sentinel 'elcord-on-connect
@@ -136,25 +133,26 @@ Argument LINE-COUNT Total number of lines in buffer."
           (set-process-query-on-exit-flag elcord-sock nil)
           (message "Sending Discord IPC handshake...")
           (elcord-send-packet 0 `(("v" . 1) ("client_id" . ,elcord-client_id)))
-          (setf elcord-connected t)))
-    (file-error (if (not elcord-connected)
+          (setq elcord-connected t)))
+    (file-error (unless elcord-connected
         (progn
           (sleep-for 1)
           (elcord-connect))))))
 
 (message "Opening Discord IPC socket...")
-(elcord-connect)
+(unless (and (boundp 'elcord-no-autoconnect) elcord-no-autoconnect)
+  (elcord-connect))
 ; (message "Hopefully connected?")
 (defvar elcord-last-known-position (count-lines (point-min) (point)))
 (defvar elcord-last-known-buffer-name (buffer-name))
 (defun elcord-command-hook ()
   "Check if we changed our current line..."
-  (if (and (or (not (eq (count-lines (point-min) (point))
+  (when (and (or (not (eq (count-lines (point-min) (point))
                         elcord-last-known-position))
                (not (string= (buffer-name) elcord-last-known-buffer-name))) elcord-connected)
       (progn
-        (setf elcord-last-known-buffer-name (buffer-name))
-        (setf elcord-last-known-position (count-lines (point-min) (point)))
+        (setq elcord-last-known-buffer-name (buffer-name))
+        (setq elcord-last-known-position (count-lines (point-min) (point)))
         (elcord-create-presence))))
 ; We have this hook which is called whenever like anything at all happens and we check if it changed the line#...
 (add-hook 'post-command-hook 'elcord-command-hook)

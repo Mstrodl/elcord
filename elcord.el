b779502a2247498e14f7975dcbcce2bd9d69887a
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2017-12-26 10:27:13 -0500

    More code cleanup
    * Get rid of unnecessary defvars
    * Mark all internal functions/variables as such (elcord-- prefix)
    * Add 'elcord-auto-reconnect' - Reconnects only if already connected and interrupted for some reason.
    * Do not try to reconnect endlessly on a call to 'elcord-connect'

--- elcord.el
+++ elcord.el
@@ -12,8 +12,9 @@
 
 ;;; Code:
 
-(require 'json)
 (eval-when-compile (require 'cl-lib))
+
+(require 'json)
 (require 'bindat)
 
 (defgroup elcord nil
@@ -27,67 +28,6 @@
                  (function :tag "Call the function with no args to get the ID."))
   :group 'elcord)
 
-(defun elcord-on-connect (process evnt)
-  "Debug function used to log connection state change.
-Argument PROCESS Process the event occured on.
-Argument EVNT The reason the connection state changed."
-  (case (process-status process)
-    (exit
-     (elcord-handle-disconnect))
-    (t
-     (message (prin1-to-string evnt)))))
-
-(defvar elcord-connected nil)
-
-(defun elcord-create-presence ()
-  "Create a new status and set it."
-  (elcord-setpresence (buffer-name) (+ 1 (count-lines (point-min) (point))) (+ 1 (count-lines (point-min) (point-max)))))
-
-(defun elcord-handle-disconnect ()
-  "Handles reconnecting when socket disconnects..."
-  (setq elcord-connected nil)
-  (setq elcord-first-message nil)
-  (elcord-connect))
-
-(defvar elcord-first-message nil)
-
-(defun elcord-on-message (process evnt)
-  "Debug function used to output all incoming packets.
-Argument PROCESS The process the message was recieved on.
-Argument EVNT The message recieved on the socket."
-  (unless elcord-first-message
-    (progn
-      (elcord-create-presence)
-      (setq elcord-first-message t))))
-
-(defvar elcord-jsonstr "")
-(defvar elcord-datalen 0)
-(defvar elcord-message-spec '())
-(defvar elcord-packet '())
-
-(defun elcord-send-packet (opcode obj)
-  "Packs and sends a packet to the IPC server.
-Argument OPCODE OP code to send.
-Argument OBJ The data to send to the IPC server."
-  (setq elcord-jsonstr (json-encode obj))
-  (setq elcord-datalen (length elcord-jsonstr))
-  (setq elcord-message-spec
-        `((:op u32r)
-          (:len u32r)
-          (:data str ,elcord-datalen)))
-  (setq elcord-packet (bindat-pack
-                       elcord-message-spec
-                       `((:op . ,opcode)
-                         (:len . ,elcord-datalen)
-                         (:data . ,elcord-jsonstr))))
-  (process-send-string elcord-sock elcord-packet))
-
-(defvar elcord-activity '())
-(defvar elcord-nonce "")
-(defvar elcord-presence '())
-(defvar elcord-pid (emacs-pid))
-(defvar elcord-join-and-spectate t)
-
 (defcustom elcord-mode-icon-alist '((emacs-lisp-mode . "emacs_icon"))
   "alist of major modes to icon names to have elcord use.
 Note, these icon names must be available as 'small_image' in Discord."
@@ -99,74 +39,64 @@ Note, these icon names must be available as 'small_image' in Discord."
   :type '(alist :key-type symbol :value-type string)
   :group 'elcord)
 
-(defun elcord--mode-icon ()
-  (let ((mode major-mode)
-        (ret "emacs_icon"))
-    (while mode
-      (if-let ((icon (assoc mode elcord-mode-icon-alist)))
-          (progn
-            (setq ret (cdr icon))
-            (setq mode nil))
-        (setq mode (get mode 'derived-mode-parent))))
+(defcustom elcord-auto-reconnect 't
+  "When enabled, elcord will automatically reconnect
+if the connection to Discord is lost."
+  :type 'boolean
+  :group 'elcord)
 
-    ret))
+(defun elcord-connect ()
+  "Connects to the Discord socket."
+  (interactive)
+  (unless (or elcord--connected
+              (null (elcord--resolve-client-id)))
+    (setq elcord--sock
+          (if (eq system-type 'windows-nt)
+              (make-process
+               :name "*elcord-sock*"
+               :command (list "pipeman" elcord--discord-ipc-pipe)
+               :connection-type nil
+               :sentinel 'elcord--connection-sentinel
+               :filter 'elcord--connection-filter)
+            (make-network-process
+             :name "*elcord-sock*"
+             :remote (concat (or (getenv "XDG_RUNTIME_DIR") (getenv "TMPDIR") (getenv "TMP") (getenv "TEMP") "/tmp") "/" elcord--discord-ipc-pipe)
+             :sentinel 'elcord--connection-sentinel
+             :filter 'elcord--connection-filter)))
+    (set-process-query-on-exit-flag elcord--sock nil)
+    (elcord--send-packet 0 `(("v" . 1) ("client_id" . ,(elcord--resolve-client-id))))
+    (setq elcord--connected t)
+    (setq elcord--elcord-update-presence-timer (run-at-time 0 15 'elcord--update-presence)))
+  elcord--connected)
 
-(defun elcord--mode-text ()
-  (let ((mode major-mode)
-        (ret mode-name))
-    (while mode
-      (if-let ((text (assoc mode elcord-mode-text-alist)))
-          (progn
-            (setq ret (cdr text))
-            (setq mode nil))
-        (setq mode (get mode 'derived-mode-parent))))
+(defun elcord-disconnect ()
+  (interactive)
+  (when elcord--connected
+    (progn
+      (when elcord--elcord-update-presence-timer
+        (cancel-timer elcord--elcord-update-presence-timer)
+        (setq elcord--elcord-update-presence-timer nil))
+      (setq elcord--first-message nil)
+      (setq elcord--connected nil)
+      (delete-process elcord--sock)
+      (setq elcord--sock nil)))
+  (when elcord--reconnect-timer
+    (cancel-timer elcord--reconnect-timer)
+    (setq elcord--reconnect-timer nil))
+  (not elcord--connected))
 
-    ret))
+(defvar elcord--discord-ipc-pipe "discord-ipc-0")
+(defvar elcord--elcord-update-presence-timer nil)
+(defvar elcord--reconnect-timer nil)
+(defvar elcord--sock nil)
+(defvar elcord--connected nil)
+(defvar elcord--first-message nil)
+(defvar elcord--last-known-position (count-lines (point-min) (point)))
+(defvar elcord--last-known-buffer-name (buffer-name))
 
-(defun elcord--mode-icon-and-text ()
-  (if-let ((icon (elcord--mode-icon))
-           (text (elcord--mode-text)))
-      (list
-       (cons "small_text" text)
-       (cons "small_image" icon))
-    nil))
-
-(defun elcord-setpresence (filename line-num line-count)
-  "Set presence.
-Argument FILENAME Name of current buffer.
-Argument LINE-NUM Line number the pointer is located at.
-Argument LINE-COUNT Total number of lines in buffer."
-  (setq elcord-activity `(
-                          ("assets" . (
-                                       ("large_image" . "emacs_icon")
-                                       ("large_text" . "Emacs")
-                                       ,@ (elcord--mode-icon-and-text)))
-                          ("details" . ,(concat "Editing " filename))
-                          ("state" . ,(concat "Line " (number-to-string line-num)))
-                          ("party" . (
-                                      ("id" . "theonlyeditor")
-                                      ("size" . [,line-num ,line-count])
-                                      ))
-                          ("secrets" . ,(if elcord-join-and-spectate '(
-                                                                       ("join" . "yesuseemacswithmepls")
-                                                                       ("match" . "emacsisbest")
-                                                                       ("spectate" . "stupidvimuseruseemacs"))
-                                          '(("match" . "emacsisbest"))))
-                          ))
-  (setq elcord-nonce (format-time-string "%s%N"))
-  (setq elcord-presence `(
-                          ("cmd" . "SET_ACTIVITY")
-                          ("args" . (("activity" . ,elcord-activity)
-                                     ("pid" . ,elcord-pid)))
-                          ("nonce" . ,elcord-nonce)
-                          ))
-  (elcord-send-packet 1 elcord-presence))
-
-(defvar elcord-discord-socket (concat (or (getenv "XDG_RUNTIME_DIR") (getenv "TMPDIR") (getenv "TMP") (getenv "TEMP") "/tmp") "/discord-ipc-0"))
-
-(defvar elcord-timer nil)
-(defvar elcord-sock nil)
 (defun elcord--resolve-client-id ()
+  "Get the client ID to use for elcord by looking at
+`elcord-client-id' and extracting its value."
   (typecase elcord-client-id
     (nil
      nil)
@@ -175,59 +105,129 @@ Argument LINE-COUNT Total number of lines in buffer."
     (function
      (funcall elcord-client-id))))
 
-(defun elcord-connect ()
-  "Connects to the Discord socket."
-  (condition-case err
-      (unless (or elcord-connected
-                  (null elcord-client-id))
-        (setq elcord-sock
-              (if (eq system-type 'windows-nt)
-                  (make-process
-                   :name "elcord-sock"
-                   :command (list "pipeman.exe" "discord-ipc-0")
-                   :connection-type nil
-                   :sentinel 'elcord-on-connect
-                   :filter 'elcord-on-message)
-                (make-network-process
-                 :name "elcord-sock"
-                 :remote elcord-discord-socket
-                 :sentinel 'elcord-on-connect
-                 :filter 'elcord-on-message)))
-        (set-process-query-on-exit-flag elcord-sock nil)
-        (message "Sending Discord IPC handshake...")
-        (elcord-send-packet 0 `(("v" . 1) ("client_id" . ,(elcord--resolve-client-id))))
-        (setq elcord-connected t)
-        (setq elcord-timer (run-at-time 0 15 'elcord-timer-hook))
-        elcord-connected)
-    (file-error
-     (run-at-time 15 nil 'elcord-connect))))
+(defun elcord--connection-sentinel (process evnt)
+  "Track connection state change on Discord connection."
+  (case (process-status process)
+    (exit
+     (elcord--handle-disconnect))
+    (t)))
 
-(defun elcord-disconnect ()
-  (when elcord-connected
+(defun elcord--connection-filter (process evnt)
+  "Track incoming data from Discord connection."
+  (unless elcord--first-message
     (progn
-      (cancel-timer elcord-timer)
-      (setq elcord-timer nil)
+      (elcord--create-presence)
+      (setq elcord--first-message t))))
 
-      (setq elcord-connected nil)
-      (delete-process elcord-sock)
-      (setq elcord-sock nil))))
+(defun elcord--reconnect ()
+  "Attempt to reconnect elcord."
+  (when elcord-auto-reconnect
+    (unless (elcord-connect)
+      (setq elcord--reconnect-timer (run-at-time 15 nil 'elcord--reconnect)))))
 
-(defvar elcord-last-known-position (count-lines (point-min) (point)))
-(defvar elcord-last-known-buffer-name (buffer-name))
+(defun elcord--handle-disconnect ()
+  "Handles reconnecting when socket disconnects..."
+  (setq elcord--connected nil)
+  (setq elcord--first-message nil)
+  (elcord--reconnect))
 
-(defun elcord-update-precense ()
+(defun elcord--create-presence ()
+  "Create a new status and set it."
+  (elcord--setpresence (buffer-name) (+ 1 (count-lines (point-min) (point))) (+ 1 (count-lines (point-min) (point-max)))))
+
+(defun elcord--send-packet (opcode obj)
+  "Packs and sends a packet to the IPC server.
+Argument OPCODE OP code to send.
+Argument OBJ The data to send to the IPC server."
+  (when (process-live-p elcord--sock)
+    (let* ((jsonstr (json-encode obj))
+           (datalen (length jsonstr))
+           (message-spec
+            `((:op u32r)
+              (:len u32r)
+              (:data str ,datalen)))
+           (packet
+            (bindat-pack
+             message-spec
+             `((:op . ,opcode)
+               (:len . ,datalen)
+               (:data . ,jsonstr)))))
+      (process-send-string elcord--sock packet))))
+
+(defun elcord--mode-icon ()
+  "Figure out what icon to use for the current major mode.
+If an icon is mapped by `elcord-mode-icon-alist', then that is used.
+Otherwise, if the mode is a derived mode, try to find an icon for it.
+If no icon is available, use the default icon."
+  (let ((mode major-mode)
+        (ret "emacs_icon"))
+    (while mode
+      (if-let ((icon (assoc mode elcord-mode-icon-alist)))
+          (progn
+            (setq ret (cdr icon))
+            (setq mode nil))
+        (setq mode (get mode 'derived-mode-parent))))
+    ret))
+
+(defun elcord--mode-text ()
+  "Figure out what text to use for the current major mode.
+If an icon is mapped by `elcord-mode-text-alist', then that is used.
+Otherwise, if the mode is a derived mode, try to find text for its parent,
+If no text is available, use the value of `mode-name'."
+  (let ((mode major-mode)
+        (ret mode-name))
+    (while mode
+      (if-let ((text (assoc mode elcord-mode-text-alist)))
+          (progn
+            (setq ret (cdr text))
+            (setq mode nil))
+        (setq mode (get mode 'derived-mode-parent))))
+    ret))
+
+(defun elcord--mode-icon-and-text ()
+  "Obtain the icon & text to use for the small icon, using current major mode.
+Get either a list
+  ((\"small_text\" . <text>) (\"small_image\" . <icon-name>))
+or nil, if no text/icon are available for the current major mode."
+  (if-let ((icon (elcord--mode-icon))
+           (text (elcord--mode-text)))
+      (list
+       (cons "small_text" text)
+       (cons "small_image" icon))
+    nil))
+
+(defun elcord--setpresence (filename line-num line-count)
+  "Set presence.
+Argument FILENAME Name of current buffer.
+Argument LINE-NUM Line number the pointer is located at.
+Argument LINE-COUNT Total number of lines in buffer."
+  (let* ((activity
+          `(("assets" . (("large_image" . "emacs_icon")
+                         ("large_text" . "Emacs")
+                         ,@(elcord--mode-icon-and-text)))
+            ("details" . ,(concat "Editing " filename))
+            ("state" . ,(concat "Line " (number-to-string line-num)))
+            ("party" . (("id" . "theonlyeditor")
+                        ("size" . [,line-num ,line-count])))
+            ("secrets" . (("match" . "emacsisbest")))))
+         (nonce (format-time-string "%s%N"))
+         (presence
+           `(("cmd" . "SET_ACTIVITY")
+             ("args" . (("activity" . ,activity)
+                        ("pid" . ,(emacs-pid))))
+             ("nonce" . ,nonce))))
+    (elcord--send-packet 1 presence)))
+
+(defun elcord--update-presence ()
   "Check if we changed our current line..."
   (when (and (or (not (eq (count-lines (point-min) (point))
-                          elcord-last-known-position))
-                 (not (string= (buffer-name) elcord-last-known-buffer-name)))
-             elcord-connected)
+                          elcord--last-known-position))
+                 (not (string= (buffer-name) elcord--last-known-buffer-name)))
+             elcord--connected)
     (progn
-      (setq elcord-last-known-buffer-name (buffer-name))
-      (setq elcord-last-known-position (count-lines (point-min) (point)))
-      (elcord-create-presence))))
-
-(defun elcord-timer-hook ()
-  (elcord-update-precense))
+      (setq elcord--last-known-buffer-name (buffer-name))
+      (setq elcord--last-known-position (count-lines (point-min) (point)))
+      (elcord--create-presence))))
 
 (provide 'elcord)
 ;;; elcord.el ends here

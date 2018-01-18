343a2bd3780a99275074e9c19199fe6d11547227
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2018-01-18 07:35:28 -0500

    Better documentation and add autoload
    * Provide documentation on the package as part of the commentary header
    * Add autoload magic comment to `elcord-mode'

--- elcord.el
+++ elcord.el
@@ -14,6 +14,15 @@
 
 ;;; Commentary:
 ;; elcord allows you to show off your buffer with all your Discord friends via the new rich presence feature
+;; To use, enable the global minor mode `elcord-mode'
+;; When enabled, elcord will communicate with the local Discord client in order to display information under
+;; the 'Playing a Game' status, updating this information at a regular interval.
+;;
+;; elcord will display an Emacs title, icon, as well as information about your current buffer:
+;; 1) The name and an icon (if available) for the current major mode
+;; 2) The name of the current buffer
+;; 3) The line number of the cursor, as well as total line count for the buffer
+;; `elcord-display-buffer-details' can be customized so that buffer name and line number are omitted.
 
 ;;; Code:
 
@@ -75,6 +84,7 @@ The mode text is the same found by `elcord-mode-text-alist'"
   :type 'boolean
   :group 'elcord)
 
+;;;###autoload
 (define-minor-mode elcord-mode
   "Global minor mode for displaying Rich Pressence in Discord."
   nil nil nil

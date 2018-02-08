cf0e5408d4f5471d70d2186d6c29239c8146dbc0
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2018-02-08 16:53:54 -0500

    Get rid of superfluous whitespace

--- stdpipe.ps1
+++ stdpipe.ps1
@@ -16,25 +16,25 @@ $stdinBuffer = new-object char[](512)
 try
 {
     $npipeClient.Connect()
- 
+
     $pipeReader = new-object System.IO.StreamReader($npipeClient)
     $pipeWriter = new-object System.IO.StreamWriter($npipeClient)
     $pipeWriter.AutoFlush = $true
-    
+
     $stdinReader = new-object System.IO.StreamReader([System.Console]::OpenStandardInput())
     $stdoutWriter = new-object System.IO.StreamWriter([System.Console]::OpenStandardOutput())
     $stdoutWriter.AutoFlush = $true
-    
+
     $taskArray =
         @(
             $pipeReader.ReadAsync($pipeBuffer, 0, $pipeBuffer.Length)
             , $stdinReader.ReadAsync($stdinBuffer, 0, $stdinBuffer.Length)
         )
-    
+
     while ($true)
     {
         $readyIndex = [System.Threading.Tasks.Task]::WaitAny($taskArray)
-        
+
         if($readyIndex -eq 0)
         {#pipe read complete
             $bytesRead = $taskArray[$readyIndex].Result
